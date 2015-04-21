3eeae2b7f0e284e29a743d902a0d5cadbb61edb6
Matthew Nichols <mnichols@umass.edu>
2015-04-20 23:26:38 -0400

    Station model

--- src/main.nim
+++ src/main.nim
@@ -3,9 +3,9 @@ import sdlx, glx, matrix, vector
 sdlx.init()
 
 var shdr = shader("flat.vert", "flat.frag")
-# var trans = identity()
-var trans = lookat(eye = vec3(0, -2, 0), target = vec3(0, -100, 100), up = vec3(0, 1, 0))
-addDraw(model("models/hind.ply", shdr, trans.addr, "bmps/notbadd.bmp"))
+var trans = identity()
+# var trans = lookat(eye = vec3(0, -2, 0), target = vec3(0, -100, 100), up = vec3(0, 1, 0))
+addDraw(model("models/station_alpha.obj", shdr, trans.addr, "bmps/notbadd.bmp"))
 
 #addDraw(rect(10,20,500,450, 0.8,0.2,0.2,1))
 #addDraw(orect(10,20,500,450, 0.2,0.2,0.2,1))
