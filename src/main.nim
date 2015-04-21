249f59623d41b13fd2b601fdb27cf342992654fb
Aaron Bentley <amanbentley@gmail.com>
2015-04-21 14:21:48 -0400

    d

--- src/main.nim
+++ src/main.nim
@@ -5,7 +5,7 @@ sdlx.init()
 var shdr = shader("flat.vert", "flat.frag")
 var trans = identity()
 # var trans = lookat(eye = vec3(0, -2, 0), target = vec3(0, -100, 100), up = vec3(0, 1, 0))
-addDraw(model("models/station_alpha.ply", shdr, trans.addr, "bmps/metal1.bmp"))
+addDraw(model("models/texturedcube.obj", shdr, trans.addr, "bmps/metal1.bmp"))
 let a = rect(10,20,500,450, 0.8,0.2,0.2,1)
 let b = orect(10,20,500,450, 0.2,0.2,0.2,1)
 let c = trect(50,30,413,397, 1,1,1,1, "bmps/notbadd.bmp")
@@ -17,7 +17,7 @@ proc drwFunc() =
   b()
   c()
 
-#addDraw(makeScreen(panel, drwFunc))
+addDraw(makeScreen(panel, a))
 #addDraw(orect(50,30,413,397, 0.2,0.2,0.2,1))
 
 sdlx.run()
